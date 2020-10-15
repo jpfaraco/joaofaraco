716271504c2433411eec3ae8d8910b53178a7148
João Faraco <jpfaraco@gmail.com>
2020-10-15 17:12:13 -0300

    adding insert 2012 release

---
+++ insert/foto.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Insert 2012</title>
+
+<style type="text/css">
+body {
+	width: 100%;
+	height: 100%;
+	background-color: #333;
+}
+
+.center {
+   width: 1024px;
+   height: 683px;
+   position: absolute;
+   left: 50%;
+   top: 50%; 
+   margin-left: -512px;
+   margin-top: -341px;
+}
+</style>
+
+</head>
+
+<body>
+<div class="center">
+	<img src="imgs/fotos/<?php echo $_GET["foto"];?>.jpg" />
+</div>
+
+</body>
+</html>
